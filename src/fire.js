e072d34c1b74594582adccd6b2a7a725954ea7c0
AlexD <alexander.dohrn@forrent.com>
2018-05-08 12:47:23 -0400

    Remove firebase data.

--- src/fire.js
+++ src/fire.js
@@ -1,11 +1,6 @@
 import firebase from 'firebase';
 const config = {
-    apiKey: "AIzaSyAfO9_eCZeiBN-b5DwQYd2yqYNOpp_nJxU",
-    authDomain: "owl-watch.firebaseapp.com",
-    databaseURL: "https://owl-watch.firebaseio.com",
-    projectId: "owl-watch",
-    storageBucket: "owl-watch.appspot.com",
-    messagingSenderId: "420356263263"
+
   };
 const fire = firebase.initializeApp(config)
 export default fire;
